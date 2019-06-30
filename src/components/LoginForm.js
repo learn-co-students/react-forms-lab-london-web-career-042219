1300884787b55dc5c9a8ce109aa0d2d345ee96e4
aidenjl193 <aidenjl193@gmail.com>
2019-06-30 20:19:06 +0100

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,29 +4,43 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+      this.state = {
+		  username: "",
+		  password: ""
+	  };
   }
 
-  render() {
-    return (
-      <form>
-        <div>
-          <label>
-            Username
-            <input id="username" name="username" type="text" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input id="password" name="password" type="password" />
-          </label>
-        </div>
-        <div>
-          <button type="submit">Log in</button>
-        </div>
-      </form>
-    );
+	onChangeHandler = (e) => {
+		this.setState({[e.target.name]: e.target.value})
+	}
+
+	handleLogin = (e) => {
+		e.preventDefault();
+		if(this.state.username != "" && this.state.password != "") {
+			this.props.handleLogin(e);
+		}
+	}
+	
+	render() {
+		return (
+			<form onSubmit={this.handleLogin}>
+				<div>
+					<label>
+						Username
+						<input id="username" name="username" type="text" onChange={this.onChangeHandler} value={this.state.username} />
+					</label>
+				</div>
+				<div>
+					<label>
+						Password
+						<input id="password" name="password" type="password" onChange={this.onChangeHandler} value={this.state.username}/>
+					</label>
+				</div>
+				<div>
+					<button type="submit">Log in</button>
+				</div>
+			</form>
+		);
   }
 }
 
